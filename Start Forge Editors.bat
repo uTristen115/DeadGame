9a64fd3e32d105352228c9da4f60093232b9a3a3
uTristen115 <228419979+uTristen115@users.noreply.github.com>
2026-06-22 13:36:02 -0500

    Dead Game

---
+++ Start Forge Editors.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+node "gunshtml\build-weapon-catalog.js"
+start "" "http://127.0.0.1:8787/forge_3_1.html"
+py -m http.server 8787 --bind 127.0.0.1
+if errorlevel 1 python -m http.server 8787 --bind 127.0.0.1
